714cf17027febea80e66a5e41a9793d590ab0a53
Jamming <Jiamingw29@gmail.com>
2020-01-06 19:11:26 -0500

    Prepare for Openrc merge

--- Team6964/src/main/java/org/igutech/autonomous/BlueRoadRunnerDepot.java
+++ Team6964/src/main/java/org/igutech/autonomous/BlueRoadRunnerDepot.java
@@ -2,14 +2,18 @@ package org.igutech.autonomous;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.drive.Drive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.path.heading.LinearInterpolator;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.igutech.autonomous.roadrunner.IguMecanumDriveBase;
+import org.igutech.autonomous.roadrunner.MecanumDriveBase;
 import org.igutech.autonomous.tuning.DashboardUtil;
 import org.igutech.autonomous.util.AutoCVUtil;
 import org.igutech.autonomous.util.AutoUtilManager;
@@ -18,17 +22,28 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import kotlin.Unit;
 
+import static org.igutech.autonomous.roadrunner.MecanumDriveBase.BASE_CONSTRAINTS;
+
 @Config
 @Autonomous(name = "BlueRoadRunnerDepot", group = "igutech")
 public class BlueRoadRunnerDepot extends LinearOpMode {
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
-    public static int pattern = 2;
+    public static int pattern = 1;
+    public static int angle = 90;
+    public static int strafe = 5;
+
+    public static int forward = 100;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        AutoUtilManager manager = new AutoUtilManager(hardwareMap, "TestRoadrunner");
+        DriveConstraints slowConstraints = new DriveConstraints(
+                35, BASE_CONSTRAINTS.maxAccel, BASE_CONSTRAINTS.maxJerk,
+                BASE_CONSTRAINTS.maxAngVel, BASE_CONSTRAINTS.maxAngAccel, BASE_CONSTRAINTS.maxAngJerk);
+
+        AutoUtilManager manager = new AutoUtilManager(hardwareMap, "BlueRoadRunnerDepot");
         manager.getDriveUtil().resetEncoders();
         IguMecanumDriveBase drive = new IguMecanumDriveBase(manager);
         drive.setPoseEstimate(new Pose2d(-33.0, 63.0, Math.toRadians(0.0)));
@@ -39,28 +54,28 @@ public class BlueRoadRunnerDepot extends LinearOpMode {
         manager.getHardware().getServos().get("GrabberServo").setPosition(0.1);
         manager.getHardware().getServos().get("CapServo").setPosition(0.53);
 
-//        manager.getCvUtil().activate();
-//        AutoCVUtil.Pattern pattern = AutoCVUtil.Pattern.UNKNOWN;
+        manager.getCvUtil().activate();
+        AutoCVUtil.Pattern pattern = AutoCVUtil.Pattern.UNKNOWN;
 
         while (!opModeIsActive() && !isStopRequested()) {
-            //AutoCVUtil.Pattern currentPattern = manager.getCvUtil().getPattern();
-//            if (!currentPattern.equals(AutoCVUtil.Pattern.UNKNOWN))
-//                pattern = currentPattern;
+            AutoCVUtil.Pattern currentPattern = manager.getCvUtil().getPattern();
+            if (!currentPattern.equals(AutoCVUtil.Pattern.UNKNOWN))
+                pattern = currentPattern;
             telemetry.addData("status", "waiting for start command...");
-//            telemetry.addData("pattern", pattern);
-//            telemetry.addData("currentPattern", currentPattern);
+            telemetry.addData("pattern", pattern);
+            telemetry.addData("currentPattern", currentPattern);
             telemetry.update();
         }
 
         if (isStopRequested()) return;
-       /* final AutoCVUtil.Pattern patternFinal = pattern;
+        final AutoCVUtil.Pattern patternFinal = pattern;
         telemetry.addData("Final Pattern", patternFinal);
         telemetry.update();
         new Thread(() -> {
             manager.getCvUtil().shutdown();
-        }).start();*/
+        }).start();
 
-        if (pattern == 1) {
+        if (pattern == AutoCVUtil.Pattern.PATTERN_A) {
             Trajectory test = drive.trajectoryBuilder()
                     .addMarker(() -> {
                         manager.getHardware().getMotors().get("left_intake").setPower(0.75);
@@ -147,27 +162,182 @@ public class BlueRoadRunnerDepot extends LinearOpMode {
             drive.followTrajectorySync(three);
         }
 
-        if (pattern == 2) {
+        if (pattern == AutoCVUtil.Pattern.PATTERN_B) {
             Trajectory patternB = drive.trajectoryBuilder()
-                    .forward(10.0)
-                    .strafeRight(15.0)
-                    .lineTo( new Vector2d(-20.0,35.0), new LinearInterpolator(Math.toRadians(0.0),Math.toRadians(-180.0)))
-                    .strafeLeft(20)
+                    .strafeRight(20.0)
+                    .forward(17.0)
+
+                    // .lineTo(new Vector2d(50.0, 35.0), new LinearInterpolator(Math.toRadians(0.0), Math.toRadians(90.0)))
                     .build();
+
             drive.followTrajectorySync(patternB);
+            drive.turnSync(Math.toRadians(178));
+            Trajectory patternB2 = drive.trajectoryBuilder()
+                    .addMarker(() -> {
+                        manager.getHardware().getMotors().get("left_intake").setPower(0.75);
+                        manager.getHardware().getMotors().get("right_intake").setPower(-0.75);
+                        return Unit.INSTANCE;
+                    })
+                    .addMarker(2.5,() -> {
+                        manager.getHardware().getServos().get("TransferServo").setPosition(0.75);
+                        return Unit.INSTANCE;
+                    })
+                    .addMarker(3.5,() -> {
+                        manager.getHardware().getServos().get("GrabberServo").setPosition(0.3);
+                        return Unit.INSTANCE;
+                    })
+
+
+                    .strafeLeft(28)
+                    .forward(5)
+                    .strafeRight(28)
+
+
+                    .back(75)
+                    .build();
+            drive.followTrajectorySync(patternB2);
+            drive.turnSync(Math.toRadians(-90));
+            Trajectory patternB3 = drive.trajectoryBuilder()
+                    .addMarker(() -> {
+                        manager.getHardware().getMotors().get("left_intake").setPower(0.0);
+                        manager.getHardware().getMotors().get("right_intake").setPower(0.0);
+                        return Unit.INSTANCE;
+                    })
+                    .back(6)
+                    .build();
+            drive.followTrajectorySync(patternB3);
+            manager.getHardware().getServos().get("FoundationServo_left").setPosition(0.6);
+            manager.getHardware().getServos().get("FoundationServo_right").setPosition(0.9);
+            drive.waitForSync(0.5);
+
+            Trajectory two = drive.trajectoryBuilder()
+                    .forward(45)
+                    .build();
+            drive.followTrajectorySync(two);
+            drive.turnSync(Math.toRadians(angle));
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.6);
+            sleep(600);
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.0);
+            sleep(400);
+            manager.getHardware().getServos().get("RotationServo").setPosition(0.95);
+            sleep(400);
+
+            manager.getHardware().getMotors().get("stoneElevator").setPower(-0.6);
+            sleep(600);
+            manager.getHardware().getServos().get("GrabberServo").setPosition(0.1);
+
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.6);
+            sleep(600);
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.0);
+            sleep(400);
+            manager.getHardware().getServos().get("RotationServo").setPosition(0.28);
+            sleep(600);
+
+            manager.getHardware().getMotors().get("stoneElevator").setPower(-0.6);
+            sleep(600);
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.0);
+            manager.getHardware().getServos().get("FoundationServo_left").setPosition(0.1);
+            manager.getHardware().getServos().get("FoundationServo_right").setPosition(0.2);
+            drive.waitForSync(0.2);
+
+            Trajectory patternB4 = new TrajectoryBuilder(drive.getPoseEstimate(), slowConstraints)
+                    .back(45)
+                    .strafeLeft(8)
+                    .forward(75)
+                    .build();
+            drive.followTrajectorySync(patternB4);
+
+
+
         }
 
-        if (pattern == 3) {
-            Trajectory patternC = drive.trajectoryBuilder()
-                    .strafeRight(3.0)
-                    .back(20.0)
-                    .strafeRight(40.0)
-                    .forward(5.0)
-                    .strafeLeft(20.0)
-                    .lineTo(new Vector2d(50.0, 35.0), new LinearInterpolator(Math.toRadians(0.0), Math.toRadians(90.0)))
+        if (pattern == AutoCVUtil.Pattern.PATTERN_C ) {
+            Trajectory patternB = drive.trajectoryBuilder()
+                    .strafeRight(20.0)
+                    .forward(10.0)
+
+                    // .lineTo(new Vector2d(50.0, 35.0), new LinearInterpolator(Math.toRadians(0.0), Math.toRadians(90.0)))
                     .build();
 
-            drive.followTrajectorySync(patternC);
+            drive.followTrajectorySync(patternB);
+
+            drive.turnSync(Math.toRadians(178));
+            Trajectory patternB2 = drive.trajectoryBuilder()
+
+                    .addMarker(() -> {
+                        manager.getHardware().getMotors().get("left_intake").setPower(0.75);
+                        manager.getHardware().getMotors().get("right_intake").setPower(-0.75);
+                        return Unit.INSTANCE;
+                    })
+                    .forward(3)
+                    .addMarker(2.5,() -> {
+                        manager.getHardware().getServos().get("TransferServo").setPosition(0.75);
+                        return Unit.INSTANCE;
+                    })
+                    .addMarker(3.5,() -> {
+                        manager.getHardware().getServos().get("GrabberServo").setPosition(0.3);
+                        return Unit.INSTANCE;
+                    })
+
+
+                    .strafeLeft(33)
+                    .forward(5)
+                    .strafeRight(28)
+
+
+                    .back(80)
+                    .build();
+            drive.followTrajectorySync(patternB2);
+            drive.turnSync(Math.toRadians(-90));
+            Trajectory patternB3 = drive.trajectoryBuilder()
+                    .addMarker(() -> {
+                        manager.getHardware().getMotors().get("left_intake").setPower(0.0);
+                        manager.getHardware().getMotors().get("right_intake").setPower(0.0);
+                        return Unit.INSTANCE;
+                    })
+                    .back(6)
+                    .build();
+            drive.followTrajectorySync(patternB3);
+            manager.getHardware().getServos().get("FoundationServo_left").setPosition(0.6);
+            manager.getHardware().getServos().get("FoundationServo_right").setPosition(0.9);
+            drive.waitForSync(0.5);
+
+            Trajectory two = drive.trajectoryBuilder()
+                    .forward(45)
+                    .build();
+            drive.followTrajectorySync(two);
+            drive.turnSync(Math.toRadians(angle));
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.6);
+            sleep(600);
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.0);
+            sleep(400);
+            manager.getHardware().getServos().get("RotationServo").setPosition(0.95);
+            sleep(400);
+
+            manager.getHardware().getMotors().get("stoneElevator").setPower(-0.6);
+            sleep(600);
+            manager.getHardware().getServos().get("GrabberServo").setPosition(0.1);
+
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.6);
+            sleep(600);
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.0);
+            sleep(400);
+            manager.getHardware().getServos().get("RotationServo").setPosition(0.28);
+            sleep(600);
+
+            manager.getHardware().getMotors().get("stoneElevator").setPower(-0.6);
+            sleep(600);
+            manager.getHardware().getMotors().get("stoneElevator").setPower(0.0);
+            manager.getHardware().getServos().get("FoundationServo_left").setPosition(0.1);
+            manager.getHardware().getServos().get("FoundationServo_right").setPosition(0.2);
+            drive.waitForSync(0.2);
+
+            Trajectory patternB4 = new TrajectoryBuilder(drive.getPoseEstimate(), slowConstraints)
+                    .back(45)
+                    .strafeLeft(8)
+                    .forward(75)
+                    .build();
+            drive.followTrajectorySync(patternB4);
         }
 
         while (!isStopRequested() && drive.isBusy()) {
--- Team6964/src/main/java/org/igutech/autonomous/util/AutoCVUtil.java
+++ Team6964/src/main/java/org/igutech/autonomous/util/AutoCVUtil.java
@@ -1,8 +1,11 @@
 package org.igutech.autonomous.util;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.igutech.autonomous.LABPipeLine;
 import org.igutech.config.Hardware;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -12,26 +15,21 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
-import org.openftc.easyopencv.OpenCvPipeline;
 
-import static org.opencv.core.CvType.CV_8UC1;
 
 public class AutoCVUtil {
     private Hardware hardware;
-     OpenCvCamera phone_camera;
+    OpenCvCamera phone_camera;
 
-     SamplePipeline stone_pipeline;
-//     FtcDashboard dashboard = FtcDashboard.getInstance();
-//     Telemetry dashboardTelemetry = dashboard.getTelemetry();
+    LABPipeLine stone_pipeline;
+    ;
     public AutoCVUtil(AutoUtilManager manager, Hardware hardware) {
         this.hardware = hardware;
 
         int cameraMonitorViewId = hardware.getHardwareMap().appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardware.getHardwareMap().appContext.getPackageName());
         phone_camera = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
-        //phone_camera = new OpenCvWebcam(hardware.getHardwareMap().get(WebcamName.class,"Webcam"),cameraMonitorViewId);
-
         phone_camera.openCameraDevice();
-        stone_pipeline = new SamplePipeline();
+        stone_pipeline = new LABPipeLine();
         phone_camera.setPipeline(stone_pipeline);
 
 
@@ -41,11 +39,11 @@ public class AutoCVUtil {
     public Pattern getPattern()
     {
         if(stone_pipeline.pattern==1)
-            return Pattern.PATTERN_C;
+            return Pattern.PATTERN_A;
         if(stone_pipeline.pattern==2)
             return Pattern.PATTERN_B;
         if(stone_pipeline.pattern==3)
-            return Pattern.PATTERN_A;
+            return Pattern.PATTERN_C;
         else
             return Pattern.UNKNOWN;
 
@@ -60,159 +58,3 @@ public class AutoCVUtil {
     }
 }
 
-class SamplePipeline extends OpenCvPipeline {
-    int left_hue;
-    int right_hue;
-
-    int left_br;
-    int right_br;
-
-    int pattern;
-    //    public static double left_one=9f / 32f;
-//    public static double left_two=(5f / 32f);
-//    public static double left_three=(15f / 32f);
-//    public static double left_four=(7f / 32f);
-//
-
-    public static int right_one = 160;
-    public static int right_two = 190;
-    public static int right_three = 210;
-    public static int right_four = 210;
-
-    public static int left_one = 90;
-    public static int left_two = 190;
-    public static int left_three = 130;
-    public static int left_four = 210;
-
-
-    FtcDashboard dashboard = FtcDashboard.getInstance();
-    Telemetry dashboardTelemetry = dashboard.getTelemetry();
-//    public static double right_one=0.5;
-//    public static double right_two=0.156;
-//    public static double right_three=0.468;
-//    public static double right_four=0.218;
-
-
-    @Override
-    public Mat processFrame(Mat input) {
-
-        input.convertTo(input, CV_8UC1, 1, 10);
-
-//        int[] left_rect = {
-//                (int) (input.cols() * left_one),
-//                (int) (input.rows() * left_two),
-//                (int) (input.cols() * left_three ),
-//                (int) (input.rows() * left_four)
-//        };
-        int[] left_rect = {
-                left_one,
-                left_two,
-                left_three,
-                left_four
-        };
-        dashboardTelemetry.addData("1", (int) (input.cols() * left_one));
-        dashboardTelemetry.addData("2", (int) (input.rows() * left_two));
-        dashboardTelemetry.addData("3", (int) (input.cols() * left_three));
-        dashboardTelemetry.addData("4", (int) (input.rows() * left_four));
-        dashboardTelemetry.update();
-
-
-//        int[] right_rect = {
-//                (int) (input.cols() * right_one),
-//                (int) (input.rows() * right_two),
-//                (int) (input.cols() * right_three),
-//                (int) (input.rows() * right_four)
-//        };
-
-        int[] right_rect = {
-                right_one,
-                right_two,
-                right_three,
-                right_four
-        };
-        Imgproc.rectangle(
-                input,
-                new Point(
-                        left_rect[0],
-                        left_rect[1]),
-
-                new Point(
-                        left_rect[2],
-                        left_rect[3]),
-                new Scalar(0, 255, 0), 1);
-
-        Imgproc.rectangle(
-                input,
-                new Point(
-                        right_rect[0],
-                        right_rect[1]),
-
-                new Point(
-                        right_rect[2],
-                        right_rect[3]),
-                new Scalar(0, 0, 255), 1);
-
-        Mat left_block = input.submat(left_rect[1], left_rect[3], left_rect[0], left_rect[2]);
-        Mat right_block = input.submat(right_rect[1], right_rect[3], right_rect[0], right_rect[2]);
-
-
-        Scalar left_mean = Core.mean(left_block);
-
-
-        Scalar right_mean = Core.mean(right_block);
-
-        left_hue = get_hue((int) left_mean.val[0], (int) left_mean.val[1], (int) left_mean.val[2]);
-        right_hue = get_hue((int) right_mean.val[0], (int) right_mean.val[1], (int) right_mean.val[2]);
-        left_br = get_brightness((int) left_mean.val[0], (int) left_mean.val[1], (int) left_mean.val[2]);
-        right_br = get_brightness((int) right_mean.val[0], (int) right_mean.val[1], (int) right_mean.val[2]);
-
-        //Patern 1 = Stone Stone Skystone
-        //Pattern 2 = Stone Skystone Stone
-        //Pattern 3 = Skystone Stone Stone
-        if (left_br > 100 && right_br > 100) pattern = 3;
-        else if (left_br > 100 && right_br < 100) pattern = 1;
-        else if (left_br < 100 && right_br > 100) pattern = 2;
-        else if (left_br < 100 && right_br < 100) {
-            if (left_br > right_br) {
-                pattern = 2;
-            } else if (left_br < right_br) {
-                pattern = 1;
-            } else {
-                pattern = 3;
-            }
-        }
-
-        return input;
-    }
-
-
-    private int get_hue(int red, int green, int blue) {
-
-        float min = Math.min(Math.min(red, green), blue);
-        float max = Math.max(Math.max(red, green), blue);
-
-        if (min == max) {
-            return 0;
-        }
-
-        float hue = 0f;
-        if (max == red) {
-            hue = (green - blue) / (max - min);
-
-        } else if (max == green) {
-            hue = 2f + (blue - red) / (max - min);
-
-        } else {
-            hue = 4f + (red - green) / (max - min);
-        }
-
-        hue = hue * 60;
-        if (hue < 0) hue = hue + 360;
-
-        return Math.round(hue);
-    }
-
-    private int get_brightness(int red, int green, int blue) {
-        return (int) (((double) (red + green + blue)) / 3);
-    }
-}
