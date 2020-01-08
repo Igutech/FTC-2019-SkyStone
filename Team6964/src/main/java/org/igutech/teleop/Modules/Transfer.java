e3d7cd9b48d4a937d11becfdb9bd6c448b1e09bd
Jiaming Wang <Jiamingw29@gmail.com>
2020-01-07 20:03:22 -0500

    Moved grabber and rotation servos to transfer module for better organization

--- Team6964/src/main/java/org/igutech/teleop/Modules/Transfer.java
+++ Team6964/src/main/java/org/igutech/teleop/Modules/Transfer.java
@@ -9,9 +9,13 @@ public class Transfer extends Module {
     private boolean toggleX = false;
     private boolean previousButtonPositionX = false;
     private boolean currentButtonPositionX = false;
+    private boolean toggleA = false;
+    private boolean toggleY = false;
+    private boolean previousButtonPositionA = false;
+    private boolean previousButtonPositionY = false;
 
     public Transfer() {
-        super(500, "FoundationServo");
+        super(500, "Transfer");
     }
 
     public void init() {
@@ -32,6 +36,32 @@ public class Transfer extends Module {
             }
         }
         previousButtonPositionX = currentButtonPositionX;
+
+        boolean currentButtonPositionA = gamepadService.getDigital(2, "a");
+
+        if (currentButtonPositionA && !previousButtonPositionA) {
+            toggleA = !toggleA;
+            if (toggleA) {
+                Teleop.getInstance().getHardware().getServos().get("GrabberServo").setPosition(0.3);
+            }
+            if (!toggleA) {
+                Teleop.getInstance().getHardware().getServos().get("GrabberServo").setPosition(0.1);
+            }
+        }
+        previousButtonPositionA = currentButtonPositionA;
+
+        boolean currentButtonPositionY = gamepadService.getDigital(2, "y");
+
+        if (currentButtonPositionY && !previousButtonPositionY) {
+            toggleY = !toggleY;
+            if (toggleY) {
+                Teleop.getInstance().getHardware().getServos().get("RotationServo").setPosition(0.95);
+            }
+            if (!toggleY) {
+                Teleop.getInstance().getHardware().getServos().get("RotationServo").setPosition(0.28);
+            }
+        }
+        previousButtonPositionY = currentButtonPositionY;
 
     }
 
